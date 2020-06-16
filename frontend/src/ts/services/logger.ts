2c93328f4002401abf0ff454fc3225a964a9498d
Günther Foidl <gue@korporal.at>
2020-06-16 22:05:29 +0200

    vue3 PoC

--- frontend/src/ts/services/logger.ts
+++ frontend/src/ts/services/logger.ts
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import { AppConfig } from "vue";
 //-----------------------------------------------------------------------------
 interface ErrorData {
     message: string;
@@ -13,24 +13,26 @@ interface Console {
 }
 //-----------------------------------------------------------------------------
 export default class Logger {
-    public static init(): void {
+    public static init(vueConfig: AppConfig): void {
         if (!__DEBUG__) {
             //const logMethodsToDisable = ["log", "info", "warn", "error", "debug"];
             const logMethodsToDisable = ["log", "debug", "time", "timeEnd"];
             Logger.removeConsoleLogging(logMethodsToDisable);
-            
-            Vue.config.errorHandler = (err, _vm, info) => {
-                const errorData: ErrorData = {
-                    message: err.message,
-                    stack  : err.stack,
-                    handler: "vue",
-                    data   : {
-                        err,
-                        info
-                    }
-                };
 
-                Logger.logUnhandledException(errorData);
+            vueConfig.errorHandler = (err, _vm, info) => {
+                if (err instanceof Error) {
+                    const errorData: ErrorData = {
+                        message: err.message,
+                        stack  : err.stack,
+                        handler: "vue",
+                        data   : {
+                            err,
+                            info
+                        }
+                    };
+
+                    Logger.logUnhandledException(errorData);
+                }
             };
 
             window.onerror = function (msg: string | Event, source: string | undefined, lineNo: number | undefined, columnNo: number | undefined, error: Error | undefined) {
