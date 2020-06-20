ab0220f6f3489587fca7e3bafba558ac92c9bcec
Günther Foidl <gue@korporal.at>
2020-06-20 19:01:13 +0200

    Loading states

--- frontend/src/ts/app.vue
+++ frontend/src/ts/app.vue
@@ -3,8 +3,13 @@
 </template>
 
 <script lang="ts">
+    import Loading      from "@cmp/loading.vue";
+    import LoadingError from "@cmp/loading-error.vue";
+
     const MainView = () => ({
-        component: import(/* webpackChunkName: "main", webpackPreload: true */ "@view/main.vue") as any     // https://github.com/vuejs/vue-class-component/issues/323#issuecomment-479834166
+        component: import(/* webpackChunkName: "main", webpackPreload: true */ "@view/main.vue") as any,    // https://github.com/vuejs/vue-class-component/issues/323#issuecomment-479834166
+        loading  : Loading,
+        error    : LoadingError
     });
     //-------------------------------------------------------------------------
     export default {
